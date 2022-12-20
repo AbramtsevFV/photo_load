0124c93e026af720115c88442b2d63dc3550cb53
AbramtsevFV <abramcev@surfstudio.ru>
2022-12-20 20:46:56 +0300

    если в простом варианте, то так

---
+++ loader/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+
+from loader.form import ImageForm
+
+
+class HomePage(View):
+    template_name = "loader/start_page.html"
+
+    def get(self, request):
+        form = ImageForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            img_obj = form.instance
+            result = 'На фото пиоза собаки'
+            return render(request, self.template_name, {'form': form, 'img_obj': img_obj, 'result': result})
