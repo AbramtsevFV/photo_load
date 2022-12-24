3192a91b2d155465784f69d7a8bc00a55258d4e1
AbramtsevFV <abramcev@surfstudio.ru>
2022-12-24 17:41:38 +0300

    one click

--- loader/form.py
+++ loader/form.py
@@ -1,10 +1,12 @@
 from django import forms
 from .models import Photo
-from django.forms import FileInput
+from django.forms import FileInput, Widget, ClearableFileInput
+
 
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Photo
         fields = ['image', ]
-        # widgets = {'image': FileInput(attrs={'class': 'btn btn-secondary'})
-        # }
+        error_messages = ''
+        widgets = {'image': FileInput(attrs={'class': "d-none", 'onchange': "form.submit()"})
+        }
